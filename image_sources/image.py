9d5f7ed1ddf78e06bbfafe568abd2a61fc558d9e
Pete Wall pwall@pivotal.io <pwall@pivotal.io>
2020-04-22 20:59:59 -0500

    Add ability to set different scaling methods for image sources

--- image_sources/image.py
+++ image_sources/image.py
@@ -1,20 +1,40 @@
+from enum import Enum, auto
 import urllib.request
 from PIL import Image
+
+from color import Color
 from image_sources.image_source import ImageSource
 
 
+class ImageScale(Enum):
+    scale = auto()
+    contain = auto()
+    cover = auto()
+
+    @classmethod
+    def all_types(cls):
+        return list(map(lambda x: x.name, list(cls)))
+
+
 class ImageContent(ImageSource):
+    scale = ImageScale.scale
     image = None
     image_url = None
 
     def get_configuration(self):
         return {
             'name': self.name,
-            'url': self.image_url
+            'url': self.image_url,
+            'scale': {
+                'value': self.scale.name,
+                'options': ImageScale.all_types()
+            }
         }
 
     def set_configuration(self, params):
         super().set_configuration(params)
+        if params.get('scale'):
+            self.scale = ImageScale[params.get('scale')]
         if params.get('url'):
             self.image_url = params.get('url')
             self.image = Image.open(urllib.request.urlopen(self.image_url))
@@ -23,4 +43,33 @@ class ImageContent(ImageSource):
         if self.image is None:
             return None
 
-        return self.image.resize(size)
+        if self.scale == ImageScale.scale:
+            return self.image.resize(size)
+
+        if self.scale == ImageScale.contain:
+            scaled = self.image.copy()
+            scaled.thumbnail(size)
+            image = Image.new(self.image.mode, size, Color.white.name)
+            image.paste(scaled, box=(
+                int((size[0] - scaled.size[0]) / 2),
+                int((size[1] - scaled.size[1]) / 2)
+            ))
+            return image
+
+        if self.scale == ImageScale.cover:
+            size_ratio = size[0] / size[1]
+            image_ratio = self.image.size[0] / self.image.size[1]
+            image_is_wider = image_ratio > size_ratio
+            image_is_taller = not image_is_wider
+            new_width = self.image.size[1] * size_ratio if image_is_wider else self.image.size[0]
+            new_height = self.image.size[0] * size_ratio if image_is_taller else self.image.size[1]
+            x_offset = int((self.image.size[0] - new_width) / 2)
+            y_offset = int((self.image.size[1] - new_height) / 2)
+            cropped = self.image.crop((
+                x_offset,
+                y_offset,
+                x_offset + new_width,
+                y_offset + new_height,
+            ))
+            return cropped.resize(size)
+        return None
--- image_sources/image_test.py
+++ image_sources/image_test.py
@@ -8,13 +8,11 @@ from image_sources.image import ImageContent
 @patch('urllib.request.urlopen')
 class TestStaticImageContent(unittest.TestCase):
     mock_image = None
-    expected_image = None
     test_fixtures_dir = os.path.join(
         os.path.dirname(os.path.realpath(__file__)), '..', 'test_fixtures'
     )
 
     def setUp(self):
-        self.expected_image = Image.open(os.path.join(self.test_fixtures_dir, 'image.png'))
         self.mock_image = open(os.path.join(self.test_fixtures_dir, 'InkywHAT-400x300.png'), 'rb')
 
     def tearDown(self):
@@ -22,12 +20,15 @@ class TestStaticImageContent(unittest.TestCase):
 
     def test_get_image(self, urlopen):
         urlopen.return_value = self.mock_image
+
         content = ImageContent({
             'url': 'http://www.example.com/images/InkywHAT-400x300.png'
         })
         image = content.get_image((400, 300))
         urlopen.assert_called_with('http://www.example.com/images/InkywHAT-400x300.png')
-        self.assertEqual(image.tobytes(), self.expected_image.tobytes())
+
+        expected_image = Image.open(os.path.join(self.test_fixtures_dir, 'image.png'))
+        self.assertEqual(image.tobytes(), expected_image.tobytes())
 
     def test_set_configuration(self, urlopen):
         urlopen.return_value = self.mock_image
@@ -45,11 +46,55 @@ class TestStaticImageContent(unittest.TestCase):
         self.assertEqual(content.get_configuration(), {
             'name': 'Test Image',
             'url': 'http://www.example.com/images/InkywHAT-400x300.png',
+            'scale': {
+                'value': 'scale',
+                'options': ['scale', 'contain', 'cover']
+            },
         })
 
         image = content.get_image((400, 300))
         urlopen.assert_called_with('http://www.example.com/images/InkywHAT-400x300.png')
-        self.assertEqual(image.tobytes(), self.expected_image.tobytes())
+
+        expected_image = Image.open(os.path.join(self.test_fixtures_dir, 'image.png'))
+        self.assertEqual(image.tobytes(), expected_image.tobytes())
+
+    def test_scaled_image(self, urlopen):
+        urlopen.return_value = self.mock_image
+
+        content = ImageContent({
+            'url': 'http://www.example.com/images/InkywHAT-400x300.png',
+            'scale': 'scale'
+        })
+        image = content.get_image((200, 200))
+        urlopen.assert_called_with('http://www.example.com/images/InkywHAT-400x300.png')
+
+        expected_image = Image.open(os.path.join(self.test_fixtures_dir, 'image_scaled.png'))
+        self.assertEqual(image.tobytes(), expected_image.tobytes())
+
+    def test_contained_image(self, urlopen):
+        urlopen.return_value = self.mock_image
+
+        content = ImageContent({
+            'url': 'http://www.example.com/images/InkywHAT-400x300.png',
+            'scale': 'contain'
+        })
+        image = content.get_image((200, 200))
+        urlopen.assert_called_with('http://www.example.com/images/InkywHAT-400x300.png')
+
+        expected_image = Image.open(os.path.join(self.test_fixtures_dir, 'image_contained.png'))
+        self.assertEqual(image.tobytes(), expected_image.tobytes())
+
+    def test_covered_image(self, urlopen):
+        urlopen.return_value = self.mock_image
+        content = ImageContent({
+            'url': 'http://www.example.com/images/InkywHAT-400x300.png',
+            'scale': 'cover'
+        })
+        image = content.get_image((200, 200))
+        urlopen.assert_called_with('http://www.example.com/images/InkywHAT-400x300.png')
+
+        expected_image = Image.open(os.path.join(self.test_fixtures_dir, 'image_covered.png'))
+        self.assertEqual(image.tobytes(), expected_image.tobytes())
 
     if __name__ == '__main__':
         unittest.main()
