e1502ae4504dd9079a07c97ff7249dc013b31a5a
Pete Wall <pete@petewall.net>
2020-11-16 19:46:33 -0600

    Set weather to be updating

--- image_sources/weather/weather.py
+++ image_sources/weather/weather.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
 import requests
-from image_sources.image_source import ImageSource
+from image_sources.periodic_updating_image_source import PeriodicUpdatingImageSource
 from color import Color
 
 FONT_PATH = os.path.join(
@@ -16,7 +16,7 @@ def get_x_position(element_width, canvas_width, offset=0):
     return int((canvas_width - element_width) / 2) + offset
 
 
-class WeatherContent(ImageSource):
+class WeatherContent(PeriodicUpdatingImageSource):
     api_key = None
     location = None
     unit = 'imperial'
@@ -26,8 +26,13 @@ class WeatherContent(ImageSource):
     condition_font = ImageFont.truetype(font=FONT_PATH, size=24)
     forecast_font = ImageFont.truetype(font=FONT_PATH, size=16)
 
+    def __init__(self, params):
+        if params.get('interval') is None:
+            params['interval'] = 3600
+        super().__init__(params)
+
     def get_configuration(self):
-        return {
+        return super().get_configuration() | {
             'name': self.name,
             'api_key': self.api_key,
             'location': self.location
@@ -55,7 +60,7 @@ class WeatherContent(ImageSource):
                 f'{forecast.status_code}: {forecast.text}')
         return current.json(), forecast.json()
 
-    def get_image(self, size):
+    def refresh_image(self, size):
         #pylint: disable=invalid-name,too-many-locals
         if self.api_key is None or self.api_key == '':
             raise ValueError('API key is required')
@@ -120,4 +125,4 @@ class WeatherContent(ImageSource):
         y = height - 70
         image_canvas.line([(0, y), (width, y)], fill=Color.black.value, width=3)
 
-        return image, None
+        return image
