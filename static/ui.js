247f14799617e612f8052ff4e10fb64b1f46fa76
Pete Wall <pete@petewall.net>
2021-07-15 19:34:38 -0500

    Update the screen

--- static/ui.js
+++ static/ui.js
@@ -74,6 +74,9 @@ function showImageSourceDetails() {
 function handleScreenEvent(data) {
   $("#screen_content .dimmer").toggleClass("active", data.screen_busy)
   $("#screen_content .dimmer").toggleClass("disabled", !data.screen_busy)
+  if (!data.screen_busy) {
+    $("#screen_content img").attr("src", `/screen/image.png?timestamp=${new Date().getTime()}`)
+  }
 }
 
 function handleSlideshowEvent(data) {
