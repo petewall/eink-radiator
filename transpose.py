4186106b2847e8ba63eb8f1efb50bef70ed2297c
Pete Wall <pete@petewall.net>
2021-08-29 20:55:22 -0500

    Add transpose, which helps define rotations and reflections

---
+++ transpose.py
@@ -0,0 +1,23 @@
+import enum
+
+from PIL import Image
+
+class Transpose(enum.Enum):
+    NONE = -1
+    FLIP_LEFT_RIGHT = Image.FLIP_LEFT_RIGHT
+    FLIP_TOP_BOTTOM = Image.FLIP_TOP_BOTTOM
+    # ROTATE_90 = Image.ROTATE_90
+    ROTATE_180 = Image.ROTATE_180
+    # ROTATE_270 = Image.ROTATE_270
+    TRANSPOSE = Image.TRANSPOSE
+    TRANSVERSE = Image.TRANSVERSE
+
+    @classmethod
+    def all_methods(cls):
+        return list(map(lambda x: x.name, list(cls)))
+
+    def apply(self, image: Image) -> Image:
+        if self == Transpose.NONE:
+            return image
+
+        return image.transpose(self.value)
